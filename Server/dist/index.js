f6a9996ec32f0e1fec0eeb213175e96795c503dc
Soroush Sajadi <soorooshsadjadee@gmail.com>
2020-10-26 09:16:32 +0100

    socket boiler plate

--- Server/dist/index.js
+++ Server/dist/index.js
@@ -1,3 +1,18 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const socket_io_1 = __importDefault(require("socket.io"));
+const http_1 = __importDefault(require("http"));
+const express = require("express");
+const route_1 = __importDefault(require("./route"));
+const app = express();
+const PORT = process.env.PORT || 5000;
+const server = http_1.default.createServer(app);
+const io = socket_io_1.default(server);
+app.use(route_1.default);
+server.listen(PORT, 
 // tslint:disable-next-line:no-console
-console.log('hi');
+() => console.log(`Server has started on port ${PORT}`));
 //# sourceMappingURL=index.js.map
--- Server/src/index.ts
+++ Server/src/index.ts
@@ -1,1 +1,24 @@
 // tslint:disable-next-line:no-console
+import { Application } from 'express';
+import socketio from "socket.io";
+import http from 'http';
+import express = require('express');
+import route from './route';
+
+const app: Application = express();
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer(app)
+const io = socketio(server);
+
+app.use(route);
+
+io.on("connection", (socket: any) => {
+    
+  });
+
+
+server.listen(PORT,
+    // tslint:disable-next-line:no-console
+    () => console.log(`Server has started on port ${PORT}`))
